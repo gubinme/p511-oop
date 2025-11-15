25d5021b4ca1c3075007ad76c12f1afd1fbb4869
User <abcd@email.com>
2025-11-15 14:41:46 +0500

    Создали класс User

--- Program.cs
+++ Program.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace p511_oop
 {
+    class User
+    {
+        public string name;
+        public string surename
+        public int age;
+        public string city;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
