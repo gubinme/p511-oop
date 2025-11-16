1620ad49d4fef0d45aca8c7bc61298266a0a5dd9
User <abcd@email.com>
2025-11-16 13:28:24 +0500

    Знакомство со студентами

--- Program.cs
+++ Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p511_oop
+namespace StudentProfiles
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Student student1 = new Student();
+
+            student1.firstName = "Джон";
+            student1.lastName = "До";
+            student1.age = 19;
+
+            Student student2 = new Student();
+
+            student2.firstName = "Том";
+            student2.lastName = "До";
+            student2.age = 20;
+
+            Console.WriteLine(" Знакомство со студентами ");
+            student1.Introduce();
+            student2.Introduce();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Привет, меня зовут {firstName} {lastName}, мне {age} лет");
         }
     }
 }
+
