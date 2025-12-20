745bd6ed5d20c83b05f9de6eaaabca64b339b58b
User <abcd@email.com>
2025-12-20 14:48:47 +0500

    WordFilter

--- Program.cs
+++ Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace p511_oop
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            Console.Write("Введите недопустимое слово: ");
+            string badWord = Console.ReadLine();
+
+            int count = 0;
+            string result = ReplaceBadWords(text, badWord, ref count);
+
+            Console.WriteLine("Результат: " + result);
+            Console.WriteLine($"Статистика: {count} замены слова {badWord}.");
+        }
+
+        static string ReplaceBadWords(string text, string badWord, ref int count)
+        {
+            string replacement = new string('*', badWord.Length);
+            string result = text.Replace(badWord, replacement);
+            count = (text.Length - result.Length) / badWord.Length;
+            return result;
         }
     }
 }
