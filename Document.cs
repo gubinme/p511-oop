4d8bb423ec7d651f88fcd1ed9e4a62a171bc7ec2
gubinme <gubin.ya@hotmail.com>
2025-12-20 17:05:03 +0500

    DocumentRendering

---
+++ Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    public abstract class Document
+    {
+        public string Author { get; set; }
+
+        public abstract void Render();
+    }
+}
---
+++ TextDocument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    public class TextDocument : Document
+    {
+        public string Content { get; set; }
+
+        public override void Render()
+        {
+            Console.WriteLine($"[Текст] Автор: {Author}");
+            Console.WriteLine($"Содержимое: {Content}");
+            Console.WriteLine("--------------------");
+        }
+    }
+}
