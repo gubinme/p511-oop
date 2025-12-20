eed96f720591c789e31f7f42c120d04d81803440
User <abcd@email.com>
2025-12-20 14:30:04 +0500

    Улучшенный калькулятор

--- Program.cs
+++ Program.cs
@@ -10,6 +10,56 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
+            ShowHeader();
+            double num1 = GetNumber("Введите первое число: ");
+            double num2 = GetNumber("Введите второе число: ");
+            char operation = GetOperation();
+            double result = Calculate(num1, num2, operation);
+            ShowResult(num1, num2, operation, result);
+        }
+
+        static void ShowHeader()
+        {
+            Console.WriteLine("--- Улучшенный калькулятор ---");
+            Console.WriteLine("Доступные операции: +, -, *, /");
+        }
+
+        static double GetNumber(string prompt)
+        {
+            Console.Write(prompt);
+            return double.Parse(Console.ReadLine());
+        }
+
+        static char GetOperation()
+        {
+            Console.Write("Введите символ операции: ");
+            return Console.ReadLine()[0];
+        }
+
+        static double Calculate(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+': return a + b;
+                case '-': return a - b;
+                case '*': return a * b;
+                case '/': return b != 0 ? a / b : double.NaN;
+                default:
+                    Console.WriteLine("Ошибка: неизвестная операция!");
+                    return double.NaN;
+            }
+        }
+
+        static void ShowResult(double a, double b, char op, double result)
+        {
+            if (double.IsNaN(result))
+            {
+                Console.WriteLine("Ошибка: невозможно выполнить операцию.");
+            }
+            else
+            {
+                Console.WriteLine($"Результат: {a} {op} {b} = {result}");
+            }
         }
     }
 }
