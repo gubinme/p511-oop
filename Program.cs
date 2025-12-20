b39952b015a92a19cb19fc10e470b7323f955acf
User <abcd@email.com>
2025-12-20 14:20:32 +0500

    Класс Rectangle.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
+            Rectangle rect1 = new Rectangle();
+            rect1.width = 10;
+            rect1.height = 5;
+
+            Rectangle rect2 = new Rectangle();
+            rect2.width = 7.5;
+            rect2.height = 3.2;
+
+            double area1 = rect1.GetArea();
+            double perimeter1 = rect1.GetPerimeter();
+
+            double area2 = rect2.GetArea();
+            double perimeter2 = rect2.GetPerimeter();
+
+            Console.WriteLine("Прямоугольник 1:");
+            Console.WriteLine($"- Ширина: {rect1.width}");
+            Console.WriteLine($"- Высота: {rect1.height}");
+            Console.WriteLine($"- Площадь: {area1}");
+            Console.WriteLine($"- Периметр: {perimeter1}");
+
+            Console.WriteLine("Прямоугольник 2:");
+            Console.WriteLine($"- Ширина: {rect2.width}");
+            Console.WriteLine($"- Высота: {rect2.height}");
+            Console.WriteLine($"- Площадь: {area2}");
+            Console.WriteLine($"- Периметр: {perimeter2}");
         }
     }
 }
---
+++ rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    public class Rectangle
+    {
+        public double width;
+        public double height;
+
+        public double GetArea()
+        {
+            return width * height;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (width + height);
+        }
+    }
+}
